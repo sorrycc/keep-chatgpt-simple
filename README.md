e0debf7712e1a181c83bdb58946041566e19fc76
sorrycc <sorrycc@gmail.com>
2023-04-19 14:29:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# keep-chatgpt-simple
+
+A simple userscript to keep chat.openai.com alive.
+
+## Install
+
+- https://github.com/sorrycc/keep-chatgpt-simple/raw/master/keep.user.js
+
+## Inspired by
+
+- https://github.com/xcanwin/KeepChatGPT
---
+++ keep.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Keep ChatGPT Simple
+// @namespace    http://sorrycc.com/
+// @version      0.1.0
+// @description  try to take over the world!
+// @author       sorrycc
+// @match        https://chat.openai.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ai.com
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(() => {
+  let apiUrl = '/api/auth/session';
+
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 11) + 20;
+  }
+
+  async function keepChat() {
+    const res = await fetch(apiUrl);
+    const contentType = res.headers.get('Content-Type');
+    const text = await res.text();
+    const isJSON = contentType && contentType.includes('application/json');
+    const is403 = res.status === 403;
+    console.log('> ', isJSON, is403, text);
+    const isValid = isJSON && !is403 && text.includes('expires');
+    if (!isValid) {
+      throw new Error('Invalid response');
+    }
+  }
+
+  function run() {
+    keepChat().then(() => {
+      const random = getRandomNumber();
+      console.log('Next run in ' + random + ' seconds');
+      setTimeout(run, random * 1000);
+    }).catch(e => {
+      console.error(e.message);
+      alert(`Session 失效了，请手动刷新页面重试！`);
+    });
+  }
+
+  run();
+})();
